b529113ded36e39b333b7df854e971262ee96c0c
BohdanBuhrii <bobuhrii@gmail.com>
2019-06-21 22:42:27 +0300

    add generic Repository

--- TaskManager/Repository/Abstract/IRepository.cs
+++ TaskManager/Repository/Abstract/IRepository.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TaskManager.Repository
+namespace TaskManager.Repository.Abstract
 {
-    interface IRepository
+    interface IRepository<T> where T : class
     {
+        IEnumerable<T> Get(Expression<Func<T,bool>> predicate);
+
+        IEnumerable<T> GetAll();
+
+        void Add(T entity);
+
+        void AddRange(IEnumerable<T> entites);
+
+        void Delete(T entity);
+
+        void DeleteRange(IEnumerable<T> entites);
+
+        void Update(T entity);
     }
 }
--- TaskManager/Repository/Repository.cs
+++ TaskManager/Repository/Repository.cs
@@ -1,12 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManager.Repository.Abstract;
 
 namespace TaskManager.Repository
 {
-    interface Repository
+    public class Repository<T> : IRepository<T> where T : class
     {
+        private readonly DbSet<T> _entities;
+
+        public Repository(DbContext dbContext)
+        {
+            _entities = dbContext.Set<T>();
+        }
+
+        public IEnumerable<T> Get(Expression<Func<T, bool>> predicate)
+        {
+            return _entities.Where(predicate);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _entities.ToList();
+        }
+
+        public void Add(T entity)
+        {
+            _entities.Add(entity);
+        }
+
+        public void AddRange(IEnumerable<T> entities)
+        {
+            _entities.AddRange(entities);
+        }
+
+        public void Delete(T entity)
+        {
+            _entities.Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            _entities.RemoveRange(entities);
+        }
+
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
