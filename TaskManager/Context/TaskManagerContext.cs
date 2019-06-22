615ae5873d43eace83e9b85c54bb820617042f3a
BohdanBuhrii <bobuhrii@gmail.com>
2019-06-22 17:55:23 +0300

    update context

--- TaskManager/Context/TaskManagerContext.cs
+++ TaskManager/Context/TaskManagerContext.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿//using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure.Annotations;
 using System.Text;
+using TaskManager.Models;
 
-namespace TaskManager.Models
+namespace TaskManager.Context
 {
     public class TaskManagerContext : DbContext
     {
@@ -15,24 +17,25 @@ namespace TaskManager.Models
         public DbSet<Task> Tasks { get; set; }
         public DbSet<Group> Groups { get; set; }
 
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)//DbModelBuilder
         {
+            #region User
             modelBuilder.Entity<User>()
                 .Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false)
                 .IsRequired();
-
-            //modelBuilder.Entity<User>()
-            //    .HasIndex(e => e.Email)
-            //    .IsUnique();
 
             modelBuilder.Entity<User>()
                  .Property(e => e.Email)
+                 .HasMaxLength(254)
+                 .IsUnicode(false)
                  .IsRequired();
-                
 
             modelBuilder.Entity<User>()
                 .Property(e => e.HashPassword)
                 .HasMaxLength(88)
+                .IsUnicode(false)
                 .IsRequired();
 
             modelBuilder.Entity<User>()
@@ -45,31 +48,54 @@ namespace TaskManager.Models
                     ug.ToTable("UserGroup");
                 });
 
+            modelBuilder.Entity<User>()
+                .HasMany(e => e.Tasks)
+                .WithRequired(e => e.Publisher);
 
-            /*
-                modelBuilder.Entity<Groups>()
-                            .HasMany(e => e.People)
-                            .WithOptional(e => e.Groups1)
-                            .HasForeignKey(e => e.GroupId)
-                            .WillCascadeOnDelete();
+            #endregion
 
-                        modelBuilder.Entity<People>()
-                            .Property(e => e.FirstName)
-                            .IsUnicode(false);
 
-                        modelBuilder.Entity<People>()
-                            .Property(e => e.LastName)
-                            .IsUnicode(false);
+            //modelBuilder.Entity<User>().HasIndex(e => e.Name);
 
-                        modelBuilder.Entity<People>()
-                            .Property(e => e.Position)
-                            .IsUnicode(false);
+            //modelBuilder.Entity<User>()
+            //    .HasIndex(e => e.Email)
+            //    .IsUnique();
 
-                        modelBuilder.Entity<People>()
-                            .HasMany(e => e.Groups)
-                            .WithRequired(e => e.People1)
-                            .HasForeignKey(e => e.MentorId)
-                            .WillCascadeOnDelete(false);*/
+
+            #region Group
+
+            modelBuilder.Entity<Group>()
+                .Property(e => e.Title)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .IsRequired();
+
+            modelBuilder.Entity<Group>()
+                .HasMany(e => e.Tasks)
+                .WithRequired(e => e.Group);
+
+            #endregion
+
+            #region Task
+
+            modelBuilder.Entity<Task>()
+                .Property(e => e.Content)
+                .HasMaxLength(500)
+                .IsUnicode(false)
+                .IsRequired();
+
+            modelBuilder.Entity<Task>()
+                .Property(e => e.PublicationDate);
+                
+            //modelBuilder.
+                        //modelBuilder.Entity<Task>()
+              //  .HasRequired(e => e.Publisher);
+                
+                
+                //.WithRequiredPrincipal(t => t.);
+
+            #endregion
+
             base.OnModelCreating(modelBuilder);
         }
     }
