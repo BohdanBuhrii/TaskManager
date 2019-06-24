b89ea92d2856753cd4afdcf2bec45b57fffe8509
BohdanBuhrii <bobuhrii@gmail.com>
2019-06-24 08:19:36 +0300

    small changes

--- TaskManager/Program.cs
+++ TaskManager/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManager.Models;
+using TaskManager.Context;
+using TaskManager.Repository;
 
 namespace TaskManager
 {
@@ -10,6 +13,12 @@ namespace TaskManager
     {
         static void Main(string[] args)
         {
+            using (UnitOfWork db = new UnitOfWork())
+            {
+                ConsoleMenu.Menu menu = new ConsoleMenu.Menu(db.UsersRepo.Get(e => e.Email=="aaa").First());
+                menu.Init();
+            }
+
         }
     }
 }
