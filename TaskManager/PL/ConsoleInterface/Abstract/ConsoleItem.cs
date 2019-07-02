1aa9ea1113bff1c442fcb13540ffe7907973b2c6
BohdanBuhrii <bobuhrii@gmail.com>
2019-07-02 13:23:31 +0300

    Add base class for all console "pages"

---
+++ TaskManager/PL/ConsoleInterface/Abstract/ConsoleItem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using TaskManager.BL.Services;
+
+namespace TaskManager.PL.ConsoleInterface.Abstract
+{
+
+    public abstract class ConsoleItem
+    {
+        protected int choise;
+        protected readonly TaskManagerProvider _provider;
+
+        public ConsoleItem()
+        {
+            choise = 0;
+            _provider = new TaskManagerProvider();
+        }
+
+        protected virtual int ReadChoise(int minValue, int maxValue)
+        {
+            int choise;
+            while (true)
+            {
+                try
+                {
+                    choise = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Incorrect input! Please try again : ");
+                    continue;
+                }
+
+                if (minValue <= choise && choise <= maxValue)
+                {
+                    return choise;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect input! Please try again : ");
+                    continue;
+                }
+            }
+        }
+
+        public virtual void Init() { }
+    }
+}
