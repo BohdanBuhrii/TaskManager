62552fd432cec654d682cc0dea56da7084cb47ed
BohdanBuhrii <bobuhrii@gmail.com>
2019-07-03 13:06:50 +0300

    Add repository for table UserGroups

---
+++ TaskManager/DAL/Repository/Abstract/AbstractsForConcrete/IUserGroupsRepo.cs
@@ -0,0 +1,8 @@
+﻿using TaskManager.DAL.Models;
+
+namespace TaskManager.DAL.Repository.Abstract.AbstractsForConcrete
+{
+    public interface IUserGroupsRepo : IRepository<UserGroup>
+    {
+    }
+}
---
+++ TaskManager/DAL/Repository/Concrete/UserGroupsRepo.cs
@@ -0,0 +1,28 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using TaskManager.DAL.Models;
+using TaskManager.DAL.Repository.Abstract.AbstractsForConcrete;
+
+namespace TaskManager.DAL.Repository.Concrete
+{
+    public class UserGroupsRepo : Repository<UserGroup>, IUserGroupsRepo
+    {
+        public UserGroupsRepo(DbContext context) : base(context) { }
+
+        
+        public override IEnumerable<UserGroup> Get(Expression<Func<UserGroup, bool>> predicate)
+        {
+            return _entities.Where(predicate).Include(ug => ug.Group).Include(ug => ug.User).AsNoTracking();
+        }
+
+        public override IEnumerable<UserGroup> GetAll()
+        {
+            return _entities.Include(ug => ug.Group).Include(ug => ug.User).AsNoTracking().ToList();
+        }
+    }
+}
