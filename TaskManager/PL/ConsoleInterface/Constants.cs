fc9abf0a867586bb4e56396c0340b4a4e2dc8b83
BohdanBuhrii <bobuhrii@gmail.com>
2019-07-07 13:08:17 +0300

    Add class with constants

---
+++ TaskManager/PL/ConsoleInterface/Constants.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManager.PL.ConsoleInterface
+{
+    public static class Constants
+    {
+        public static int ConsoleWidth
+        {
+            get { return 40; }
+        }
+
+        public static char HorizontalSeparator
+        {
+            get { return '='; }
+        }
+
+        public static char VerticalSeparator
+        {
+            get { return '|'; }
+        }
+    }
+}
