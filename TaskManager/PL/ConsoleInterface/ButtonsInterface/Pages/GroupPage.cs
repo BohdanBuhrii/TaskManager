60b0b0eece1b45bf549b63dbf29dbed41c043254
BohdanBuhrii <bobuhrii@gmail.com>
2019-07-07 20:32:09 +0300

    Implement more features

--- TaskManager/PL/ConsoleInterface/ButtonsInterface/Pages/GroupPage.cs
+++ TaskManager/PL/ConsoleInterface/ButtonsInterface/Pages/GroupPage.cs
@@ -5,7 +5,7 @@ using TaskManager.BL.Services;
 
 namespace TaskManager.PL.ConsoleInterface.ButtonsInterface.Pages
 {
-    public class GroupPage : Page
+    public class GroupPage : ListView
     {
         private List<TaskDTO> _tasks;
         private readonly GroupDTO _group;
@@ -41,7 +41,8 @@ namespace TaskManager.PL.ConsoleInterface.ButtonsInterface.Pages
             }
 
             buttons.Add(new Button { Content = "Add new task", ButtonAction = AddNewTask, AbilityToChange = false });
-            buttons.Add(new Button { Content = "Add new user", ButtonAction = AddNewUser, AbilityToChange = false });
+            buttons.Add(new Button { Content = "Add new user", ButtonAction = AddNewUserToGroup, AbilityToChange = false });
+            buttons.Add(new Button { Content = "Leave group", ButtonAction = LeaveGroup, AbilityToChange = false });
             buttons.Add(new Button { Content = "<-- back to groups", ButtonAction = BackToGroups, AbilityToChange = false });
         }
 
@@ -53,20 +54,65 @@ namespace TaskManager.PL.ConsoleInterface.ButtonsInterface.Pages
             task.PublicationDate = DateTime.Now;
             _provider.AddNewTask(task, _user, _group);
 
-
             ShowSuccessMessage("Task created successfully! Press any key to continue");
             Console.ReadKey();
-            this.Init();
+            GroupPage groupPage = new GroupPage(_group, _user);
+            groupPage.Init();
         }
 
-        public void AddNewUser()
+        public void AddNewUserToGroup()
         {
+            while (true)
+            {
+                Console.Write("Enter user email : ");
+                string email = Console.ReadLine();
+                if (_provider.IsEmailExist(email))
+                {
+                    UserDTO newMember = _provider.GetUserByEmail(email);
 
+                    _provider.AddUserToGroup(newMember, _group);
+
+                    ShowSuccessMessage($"{newMember.Name} successfully joined to group, press any key to continue");
+                    Console.ReadKey();
+                    buttons[CurrentPosition].ToUsual();
+                    _currentPosition = 0;
+                    this.Init();
+                    break;
+                }
+                else
+                {
+                    ShowErrorMessage("User with this email not found, press Enter to try again, press any other key to go back");
+
+                    if (Console.ReadKey().Key != ConsoleKey.Enter)
+                    {
+
+                        buttons[CurrentPosition].ToUsual();
+                        _currentPosition = 0;
+                        this.Init();
+                        break;
+                    }
+                    else
+                    {
+                        //Console.WriteLine();
+                    }
+                }
+            }
         }
 
         public void MarkTaskAsDone()
         {
+            TaskDTO task = _tasks[CurrentPosition - 1];
+            _provider.MarkTaskAsDone(task);
 
+            GroupPage groupPage = new GroupPage(_group, _user);
+            groupPage.Init();
+        }
+
+        public void LeaveGroup()
+        {
+            _provider.DeleteUserFromGroup(_user, _group);
+            GroupListPage groupListPage = new GroupListPage(_user);
+            groupListPage.Init();
         }
 
         public void BackToGroups()
